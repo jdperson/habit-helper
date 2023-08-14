d4cc7bdebca3a54c226a894be477f9a98ca65ffe
JD <jondavidperson@gmail.com>
2023-08-14 13:12:10 -0500

    fixed export form models/index, fixed indents on homeRoutes, modified loginBtn listener and started populateFriends() in public/index.js

--- public/js/index.js
+++ public/js/index.js
@@ -23,6 +23,25 @@ const addFriendBtn = document.getElementsByClassName('add-friend-btn');
 
 // ================== FUNC DEFS ==================
 
+async function populateFriends(user) {
+    try {
+        const response = await fetch('api/friendRequest/', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(user.id)
+        });
+
+        // Response will return all friend requests linked to this user.
+        //
+        // Display all with status 'accepted' to the friends list
+        //
+        // Display all incoming pending requests to requests list
+        // incoming request => other_id == user.id
+    } catch (err) {
+        console.log('Error retrieving friends', err);
+    }
+}
+
 // ===============================================
 
 
@@ -35,38 +54,47 @@ const addFriendBtn = document.getElementsByClassName('add-friend-btn');
 
 // ================== LISTENERS ==================
 
-const user = loginBtn.addEventListener('submit', async (e) => {
+loginBtn.addEventListener('submit', async (e) => {
     try {
         const response = await fetch('/api/user/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(email, password)
         });
-        return response;
+        
+        // Send user to the dashboard, generate their tracking graph,
+        // and populate their friends list + friend requests
+
+        // window.location.replace('dashboard-link')
+        // generateGraph(response.user)
+        populateFriends(response.user);
     } catch (err) {
         console.log('Error logging in: ', err);
     }
 });
 
 addFriendBtn.addEventListener('submit', async (e) => {
-    const newFriend = await User.findOne({ 
-        where: { username: e.target.closest('a').textContent }
-    });
-    
-    if (newFriend) {
-        try {
-            const response = await fetch('/api/user/addFriend', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(user.id, newFriend.id)
-            });
+    try {
+        const newFriend = await fetch(`/api/user/${e.target.closest('input').textContent}`);
 
-            if (response) { console.log('Friend request sent'); }
-
-        } catch (err) {
-            console.log('Error adding friend: ', err);
+        if (newFriend) {
+            try {
+                const response = await fetch('/api/friendRequest/addFriend', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(user.id, newFriend.id)
+                });
+        
+                if (response) { console.log('Friend request sent'); }
+        
+            } catch (err) {
+                console.log('Error adding friend: ', err);
+            }
         }
-    };
+    } catch (err) {
+        console.log(`Could not find user with name ${e.target.closest('input').textContent}`);
+    }
+    
 });
 
 // ===============================================
