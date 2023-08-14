33ed6f6ef455a9f5123766c02f40443022b0b3d7
ColtonSiegmund <135175571+ColtonSiegmund@users.noreply.github.com>
2023-08-14 11:29:55 -0500

    added homeRoutes and renders for tips, habits and users

--- controllers/api/habit-routes.js
+++ controllers/api/habit-routes.js
@@ -2,42 +2,6 @@ const router = require('express').Router();
 const { Habit, Tip} = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-  try {
-    const habitData = await Habit.findAll({
-      include: [{ model: Tip, as: 'tip' }]
-    });
-    res.status(200).json(habitData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/:id', withAuth, async (req, res) => {
-  try {
-    const habitData = await Habit.findByPk(req.params.id, {
-      include: [{ model: Tip, as: 'tip' }]
-    });
-
-    if (!habitData) {
-      res.status(404).json({ message: 'No habit found with this id!' });
-      return;
-    }
-
-    res.status(200).json(habitData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const habitData = await Habit.create(req.body);
-    res.status(200).json(habitData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
 
 router.put('/:id', withAuth, async (req, res) => {
   try {
